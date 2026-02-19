964fb7c5950376d7b1b2100618a10e74f2ce182b
PedroGiorlando <pggv2003@gmail.com>
2026-02-19 19:56:23 -0300

    Test: Forzar URL de Render en el front

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+    baseURL: 'https://biblioteca-digital-fi5y.onrender.com/api',
 });
 
 export default api;
